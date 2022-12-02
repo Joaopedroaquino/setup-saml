9503867040090f1e48e29a8f64029ddc3f7faf07
joaopedroaquino <aqerhack@gmail.com>
2022-12-02 13:41:47 -0300

    inj: :syringe: inject dependencies in controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthService } from './modules/auth/auth.service';
+import { SamlStrategy } from './modules/auth/strategy/saml-strategy';
+import { UserUseCase } from './modules/user/useCases/userUseCase';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,
+    private authService: AuthService,
+    private userUsecase: UserUseCase,
+    private samlStrategy: SamlStrategy 
+    ) {}
 
   @Get()
   getHello(): string {
