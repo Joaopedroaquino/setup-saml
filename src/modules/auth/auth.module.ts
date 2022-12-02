e8f574cab6d22245f8b018c530828a75990e101e
joaopedroaquino <aqerhack@gmail.com>
2022-12-02 13:24:22 -0300

    init: :construction: init auth-module config

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,4 +1,24 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { SamlStrategy } from './strategy/saml-strategy';
 
-@Module({})
+@Module({
+    imports: [
+        PassportModule,
+        JwtModule.register({
+            secret: 'test',
+            signOptions: { expiresIn: '60s'}
+
+        })
+    ],
+
+    providers: [
+        AuthService,
+        SamlStrategy
+    ],
+
+    exports: [AuthService, SamlStrategy]
+})
 export class AuthModule {}
