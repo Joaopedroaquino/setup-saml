e338d84b72592b2f49f884b4b107993323f06829
joaopedroaquino <aqerhack@gmail.com>
2023-01-10 10:58:10 -0300

    init auth-service

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { User } from "../user/contracts/entities/user";
+import { UserToken } from "../users/contracts/dtos/UserTokenDto";
 import { Users } from "../users/contracts/entities/users";
 
 @Injectable()
@@ -17,4 +18,20 @@ export class AuthService {
 
     return this.jwtService.sign(payload);
   }
+
+  async login(user: LogingDto):Promise<UserToken> {
+    const payload: UserPayload = {
+      sub:user.id,
+      emailcorporative: user.emailcorporative
+    }
+    const userNotExist = await this.userService.findByEmail(user.emailcorporative)
+    if(!userNotExist?.id){
+      throw new NotFoundException("User not exist Or Email incorrect!")
+      
+    }
+    return {
+      access_token: this.jwtService.sign(payload)
+    }
+  }
+
 }
