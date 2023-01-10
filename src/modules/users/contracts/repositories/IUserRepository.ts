ee44be9b97463858f60dcfb74468decabaa2f945
joaopedroaquino <aqerhack@gmail.com>
2023-01-10 12:50:56 -0300

    create user-service

--- src/modules/users/contracts/repositories/IUserRepository.ts
+++ src/modules/users/contracts/repositories/IUserRepository.ts
@@ -1,8 +1,9 @@
 import { User } from "src/modules/user/contracts/entities/user";
+import { Users } from "../entities/users";
 
 
 export interface IUserRepository {
 
-   findByEmail(emailcorporative: string): Promise<User>
+   findByEmail(emailcorporative: string): Promise<Users>
    
  }
