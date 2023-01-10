a208ef68c4292505a2cd64220337711ae91e0a1d
joaopedroaquino <aqerhack@gmail.com>
2023-01-10 15:48:48 -0300

    config auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,11 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { UserUseCase } from '../user/useCases/userUseCase';
 import { UserModule } from '../user/user.module';
 import { UserTypeORMRepository } from '../users/infra/typeorm/repositories/UserTypeORMRepository';
+import { RoutesModule } from '../users/users.module';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './guards/jwt-auth-guard';
 import { SamlAuthGuard } from './guards/saml-auth-guard';
@@ -13,12 +16,14 @@ import { jwtConstants } from './utils/constants';
 @Module({
     imports: [
         PassportModule,
+        ConfigModule.forRoot(),
+        forwardRef(() => RoutesModule),
+        PassportModule,
         JwtModule.register({
-            secret:  jwtConstants.secret,
-            signOptions: { expiresIn: '60s'}
-
+          secret: process.env.JWT_SECRET,
+          signOptions: { expiresIn:'1d'},
         }),
-        UserModule,
+        RoutesModule,
         
     ],
 
@@ -28,7 +33,8 @@ import { jwtConstants } from './utils/constants';
         SamlAuthGuard,
         JwtStrategy,
         JwtAuthGuard,
-        UserTypeORMRepository
+        UserTypeORMRepository, 
+        UserUseCase
     ],
 
     exports: [AuthService, SamlStrategy]
